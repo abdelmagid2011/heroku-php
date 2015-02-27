7634271a4d2e576d5d84492b2bb8a846d2f5208a
abdelmagid <abdelmagid@clavisone.com>
2015-02-27 20:37:46 +0300

    assets

--- web/index.php
+++ web/index.php
@@ -31,6 +31,18 @@ $app->post('/paymentCallback.php', function(Request $request) use($app) {
   $app['monolog']->addDebug('logging output.');
     return new Response('paymentCallback.php');
 });
+
+$app->get('/assets/{name}', function( $name,Request $request ) use ( $app ) {
+	$full_name = ''.$name;
+	if ( !file_exists( $full_name ) )
+	{
+		throw new \Exception( 'File not found' );
+	}
+	$data = file_get_contents($storage_path . $thumb_name);
+	$out = new Response($data, 200, array('Content-type' => 'image/jpeg'));
+	return $out;
+});
+
 $app->run();
 
 ?>

82bcd369d89d6418b5ac713cdb347d7e7806aa2b
abdelmagid <abdelmagid@clavisone.com>
2015-02-27 17:08:05 +0300

    welcome

--- web/index.php
+++ web/index.php
@@ -2,6 +2,9 @@
 	<header>
 	</header/>
 	<body>
+		<div class="fb-like" data-send="true" data-width="450" data-show-faces="true">
+		</div>
+		<h1 id="fb-welcome"></h1>
 		<script>
 		  window.fbAsyncInit = function() {
 			FB.init({
@@ -20,8 +23,28 @@
 			 js.src = "//connect.facebook.net/en_US/sdk.js";
 			 fjs.parentNode.insertBefore(js, fjs);
 		   }(document, 'script', 'facebook-jssdk'));
+		   
+		   function onLogin(response) {
+			  if (response.status == 'connected') {
+				FB.api('/me?fields=first_name', function(data) {
+				  var welcomeBlock = document.getElementById('fb-welcome');
+				  welcomeBlock.innerHTML = 'Hello, ' + data.first_name + '!';
+				});
+			  }
+			}
+
+			FB.getLoginStatus(function(response) {
+			  // Check login status on load, and if the user is
+			  // already logged in, go directly to the welcome message.
+			  if (response.status == 'connected') {
+				onLogin(response);
+			  } else {
+				// Otherwise, show Login dialog first.
+				FB.login(function(response) {
+				  onLogin(response);
+				}, {scope: 'user_friends, email'});
+			  }
+			});
 		</script>
-		<div class="fb-like" data-send="true" data-width="450" data-show-faces="true">
-		</div>
 	</body>
 </html>

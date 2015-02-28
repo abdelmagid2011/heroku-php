5890f9f0a5c198028cadf0c9c6342ec57a3e7035
abdelmagid <abdelmagid@clavisone.com>
2015-02-28 11:28:58 +0300

    split scripts

--- web/scripts/scripts.js
+++ web/scripts/scripts.js
@@ -1,109 +1,83 @@
 //create js element in header
-loadJS=function(src){
-	var ga = document.createElement('script'); 
-	ga.type = 'text/javascript'; 
-	//ga.async = true;
-	ga.src = src;
-	var s = document.getElementsByTagName('script')[0]; 
-	s.parentNode.insertBefore(ga, s);
-}
+var fbLike = document.getElementById("fbLike");
+fbLike.style.visibility="hidden";
+//'404839423029580'	
+initApp=function(request){
+	window.fbAsyncInit = function() {
+		FB.init({
+		  appId      : request.app_id,
+		  xfbml      : true,
+		  version    : 'v2.2'
+		});
+	  (function(d, s, id){
+		 var js, fjs = d.getElementsByTagName(s)[0];
+		 if (d.getElementById(id)) {return;}
+		 js = d.createElement(s); js.id = id;
+		 js.src = "//connect.facebook.net/en_US/sdk.js";
+		 fjs.parentNode.insertBefore(js, fjs);
+	   }(document, 'script', 'facebook-jssdk'));
+	}
+};
+
+onLogin=function(response) {
+  if (response.status == 'connected') {
+	FB.api('/me?fields=first_name', function(data) {
+	  var welcomeBlock = document.getElementById('fb-welcome');
+	  welcomeBlock.innerHTML = 'Hello, ' + data.first_name + '!';
+	});
+  }
+};
+getLoginStatus=function(request){
+FB.getLoginStatus(function(response) {
+  // Check login status on load, and if the user is
+  // already logged in, go directly to the welcome message.
+  if (response.status == 'connected') {
+	onLogin(response);
+  } else {
+	// Otherwise, show Login dialog first.
+	FB.login(function(response) {
+	  onLogin(response);
+	}, {scope: request.scope});
+  }
+});
+};
+
 //log our data to console
 logMe=function(val){
-	//console.log(val);
+	console.log(val);
 };
 
 //swf object load the game
-loadGame=function(param){
-var params = {params:param};
-params.quality = "high";
-params.bgcolor = "#000000";
-params.wmode='opaque';
-params.allowscriptaccess = "always";
-params.menu = false;
-var attributes = {};
-attributes.name = "flashContent";
-attributes.align = "middle";
-swfobject.embedSWF("SpongePoker_2014_10_16_1003.swf", "flashContent", "770", "830","10.2", params,params, params, attributes); 	
-}	
-//like our page	
-viewLikeOld=function (liked){
-	//logMe("liked liked");
-	liked=liked||"";
-	var fjs = document.getElementById("flashContent");
-	var js = document.createElement("iframe");
-	js.src = "like.html";
-	js.frameBorder ='0' ;
-	
-	var dv = document.createElement("div");
-	js.width='130';
-	js.height="33";
-	js.scrolling='no';
-	dv.className="fixedPos";
-	dv.id="fbLike";
+loadGame=function(){
+	params.quality = "high";
+	params.bgcolor = "#000000";
+	params.wmode='opaque';
+	params.allowscriptaccess = "always";
+	params.allowFullScreen = "true";
+	params.allowFullScreenInteractive = "true";
+	params.menu = false;
+	var attributes = {};
+	attributes.name = "flashContent";
+	attributes.align = "middle";
+	swfobject.embedSWF("flashSwf.swf", "flashContent", null,null,"10.2", params,params, params, attributes); 	
+};	
 
-	var cen=document.createElement("div");
-	cen.className="innerDiv";
-	cen.appendChild(js);
-	dv.appendChild(cen);
-	cen.style.visibility="hidden";
-	//console.log("like it111");
-	js.onload=onFrameLoaded(cen);
-	document.body.appendChild(dv, fjs);
-};
-viewLike=function (liked){
+viewLike=function (request){
 	//logMe("liked liked");
 	var fbLike = document.getElementById("fbLike");
 	fbLike.style.visibility="visible";
-	//alert("visible"+liked);
+	if(request!=null)
+		fblike.style.pointerEvents='none';
+		
 	FB.Event.subscribe('edge.create',function(response) {
-			//var fbLike = document.getElementById("fbLike");
-			
-			fbLike.style.visibility="hidden";
-			//alert("like clicked");
-			window.document.flashContent.likeClicked();
+			//flashContentCallback(response);
+			logMe('liked');
+			fblike.style.pointerEvents='none';
 		}
 	);
 };
-
-onFrameLoaded=function(cen){
-	//console.log("like it");
-	//cen.style.visibility="visible";
-	setTimeout(function(){cen.style.visibility="visible";},1000);
-};
-
-//check problem in facebook sdk reload
-verifyFB = function (){
-	var xmlhttp;
-	if (window.XMLHttpRequest)
-	{// code for IE7+, Firefox, Chrome, Opera, Safari
-		xmlhttp=new XMLHttpRequest();
-	}
-	else
-	{// code for IE6, IE5
-		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-	}
-	xmlhttp.onreadystatechange=function()
-	{
-		if (xmlhttp.readyState==4 && xmlhttp.status==200)
-		{
-			console.log(xmlhttp.responseText);
-			if(xmlhttp.responseText=="[true]"){
-				//alert(xmlhttp.responseText);
-				//reloadPage();
-				
-			}
-		}
-		
-	}
-	xmlhttp.open("GET","/game/facebook/userInfo.php",false);
-	xmlhttp.send();
-};
-setTimeout(verifyFB,1000);
-
 //reload the page
 reloadPage=function(){
-	
-	
 	if (window.XMLHttpRequest)
 	{// code for IE7+, Firefox, Chrome, Opera, Safari
 		top.window.location.href="//apps.facebook.com/spongepoker/?start_session=true";
@@ -115,7 +89,6 @@ reloadPage=function(){
 		self.parent.location.reload();
 	}
 };
-
-//google analytics
-
-
+flashContentCallback=function(response){
+	window.document.flashContent.callback(response
+};
